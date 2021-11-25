97b557260f96315886f92f0068085b68541d9cde
AOCHOAG <aochoag@alumnos.nebrija.es>
2021-11-25 15:25:16 +0100

    Ejercicio 9 Working

--- main.py
+++
@@ -1,4 +0,0 @@
-numero1 = float(input("Introduce un número:"))
-numero2 = float(input("Introduce otro numero al que quieres elevar el anterior: "))
-resultado = numero1**numero2
-print("El resultado es:", resultado)
